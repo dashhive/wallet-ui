1a2c7b7d1108fdeae91807ac56c1566aad700a4b
jojobyte <184880+jojobyte@users.noreply.github.com>
2023-07-19 21:29:16 -0600

    build: :rocket: Add github workflow deploy

---
+++ .github/workflows/deploy.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env -S node --no-warnings
+
+process.removeAllListeners('warning');
+process.on('SIGTERM', () => process.exit());
+process.on('SIGINT', () => process.exit());
+
+import { copyFile, readdir, mkdir } from 'node:fs/promises';
+import { basename } from 'node:path';
+
+try {
+  const files = await readdir('./pages');
+  for (const file of files) {
+    if (file.endsWith('.html')) {
+      const name = basename(file, '.html');
+      await mkdir(`./${name}`, { recursive: true });
+      await copyFile(`./pages/${file}`, `./${name}/index.html`);
+      console.log(file, name, file.endsWith('.html'), name.includes('.html'));
+    }
+  }
+
+  console.log('replaced named html files with {name}/index.html!');
+} catch (err) {
+  console.error(err);
+}
