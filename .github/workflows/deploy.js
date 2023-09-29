b2da52563985c8c04532d79856cb313bf178ca1f
jojobyte <184880+jojobyte@users.noreply.github.com>
2023-09-29 17:01:53 -0600

    ci: :green_heart: debug github action

--- .github/workflows/deploy.js
+++ .github/workflows/deploy.js
@@ -10,12 +10,17 @@ import { basename } from 'node:path';
 try {
   const files = await readdir('./');
   for (const file of files) {
-    if (file.endsWith('.html')) {
-      const name = basename(file, '.html');
-      await mkdir(`./${name}`, { recursive: true });
-      await copyFile(`./${file}`, `./${name}/index.html`);
-      console.log(file, name, file.endsWith('.html'), name.includes('.html'));
-    }
+    // if (file.endsWith('.html')) {
+      // const name = basename(file, '.html');
+      // await mkdir(`./${name}`, { recursive: true });
+      // // await copyFile(`./src/${file}`, `./${name}/index.html`);
+      // await copyFile(`./src/${file}`, `./${name}`);
+      console.log(
+        file, name,
+        file.endsWith('.html'),
+        name.includes('.html')
+      );
+    // }
   }
 
   console.log('replaced named html files with {name}/index.html!');
