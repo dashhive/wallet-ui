810cb7b02c7ed87a3f111b6dea23bba5963625f9
jojobyte <184880+jojobyte@users.noreply.github.com>
2023-09-29 23:24:08 -0600

    build: :green_heart: fix github pages deploy

--- .github/workflows/deploy.js
+++ .github/workflows/deploy.js
@@ -4,26 +4,46 @@ process.removeAllListeners('warning');
 process.on('SIGTERM', () => process.exit());
 process.on('SIGINT', () => process.exit());
 
-import { copyFile, readdir, mkdir } from 'node:fs/promises';
-import { basename } from 'node:path';
+import { readdir, cp, } from 'node:fs/promises';
+import { join, } from 'node:path';
 
 try {
-  const files = await readdir('./');
-  for (const file of files) {
-    // if (file.endsWith('.html')) {
-      // const name = basename(file, '.html');
-      // await mkdir(`./${name}`, { recursive: true });
-      // // await copyFile(`./src/${file}`, `./${name}/index.html`);
-      // await copyFile(`./src/${file}`, `./${name}`);
-      console.log(
-        file, name,
-        file.endsWith('.html'),
-        name.includes('.html')
-      );
-    // }
-  }
+  const walk = async (dirPath, filterFiles = f => f) => Promise.all(
+    await readdir(
+      dirPath,
+      { withFileTypes: true }
+    )
+    .then((entries) => entries.filter(filterFiles).map((entry) => {
+      const childPath = join(dirPath, entry.name)
+      return entry.isDirectory() ? walk(childPath) : childPath
+    })),
+  )
 
-  console.log('replaced named html files with {name}/index.html!');
+  let allFiles = await walk(
+    './',
+    file => !file?.name?.includes('.git/')
+      // && !file?.name?.startsWith('node_modules'),
+  )
+  allFiles = allFiles.flat(Number.POSITIVE_INFINITY)
+
+  allFiles.forEach(e => {
+    let fixDir = e?.split('src/')
+
+    // console.log('fixDir', fixDir, dirname(e), basename(e))
+
+    if (fixDir.length > 1) {
+      cp(e, fixDir[1], { recursive: true, });
+      return fixDir[1]
+    }
+
+    return e
+  })
+
+
+  console.log(
+    'replaced named html files with {name}/index.html!',
+    allFiles
+  );
 } catch (err) {
   console.error(err);
 }
